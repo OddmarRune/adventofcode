d68b74415339d5adbc90974d1279af9f7f2b1325
Oddmar Eiksund <oddmar.eiksund@gmail.com>
2023-12-10 22:32:16 +0100

    Simplifying, but is it really simpler?

--- 2023/Dec10/dec10_part1.py
+++ 2023/Dec10/dec10_part1.py
@@ -1,47 +1,33 @@
 """Advent of Code, 2023, Day 10"""
 
-def initial(table, row, col):
-    """Find an initial direction"""
-    rows = len(table)
-    cols = len(table[0])
-    if 0 < row < rows and table[row-1][col] in '|F7':
-        return 0, row-1, col # up
-    elif 0 < col < cols and table[row][col-1] in '-LF':
-        return 1, row, col-1 # left
-    elif 0 <= col < cols-1 and table[row][col+1] in '-J7':
-        return 2, row, col+1 # right
-    elif 0 <= row < rows-1 and table[row+1][col] in '|JL':
-        return 3, row+1, col # down
-    return -1, row, col
-
 DIRECTIONS = ["|7F ", "L- F", "J -7", " JL|"]
 DR = [-1, 0, 0, 1]
 DC = [ 0,-1, 1, 0]
-
-def follow(table, row, col, direction):
-    """Follow the loop"""
-    next_direction = DIRECTIONS[direction].find(table[row][col])    
-    return next_direction, row+DR[next_direction], col+DC[next_direction]
 
 def main():
     """Solution to part 1"""
     with open('input.txt', encoding="UTF-8") as file:
         table = []
-        rows = 0
         for line in file.readlines():
             table.append(line.strip())
             if 'S' in line:
-                start = (rows, line.find('S'))
-            rows += 1
+                row, col = len(table), line.find('S')
+        rows = len(table)
+        cols = len(table[0])
+        for direction in range(4):
+            if 0 <= row+DR[direction] < rows and 0 <= col+DC[direction] < cols \
+                    and table[row+DR[direction]][col+DC[direction]] in DIRECTIONS[direction]:
+                row += DR[direction]
+                col += DC[direction]
+                break
         steps = 1
-        row, col = start
-        print(f"start : {row}, {col}, {table[row][col]}")
-        direction, row, col = initial(table, row, col)
         while table[row][col] != 'S':
-            direction, row, col = follow(table, row, col, direction)
-            steps += 1
+            direction = DIRECTIONS[direction].find(table[row][col])
             if direction<0:
                 break
+            row += DR[direction]
+            col += DC[direction]
+            steps += 1
 
         print(f"steps to the middle of loop : {int(steps/2)}") # 7173
 
--- 2023/Dec10/dec10_part2.py
+++ 2023/Dec10/dec10_part2.py
@@ -1,27 +1,8 @@
 """Advent of Code, 2023, Day 10"""
 
-def initial(table, row, col):
-    """Find an initial direction"""
-    rows = len(table)
-    cols = len(table[0])
-    if 0 < row < rows and table[row-1][col] in '|F7':
-        return 0, row-1, col # up
-    elif 0 < col < cols and table[row][col-1] in '-LF':
-        return 1, row, col-1 # left
-    elif 0 <= col < cols-1 and table[row][col+1] in '-J7':
-        return 2, row, col+1 # right
-    elif 0 <= row < rows-1 and table[row+1][col] in '|JL':
-        return 3, row+1, col # down
-    return -1, row, col
-
-DIRECTIONS = ["|7F ", "L- F", "J -7", " JL|"]
+DIRECTIONS = ["|7FX", "L-XF", "JX-7", "XJL|"]
 DR = [-1, 0, 0, 1]
 DC = [ 0,-1, 1, 0]
-
-def follow(table, row, col, direction):
-    """Follow the loop"""
-    next_direction = DIRECTIONS[direction].find(table[row][col])
-    return next_direction, row+DR[next_direction], col+DC[next_direction]
 
 def main():
     """Solution to part 2"""
@@ -29,24 +10,31 @@ def main():
         table = []
         data = []
         output = []
-        rows = 0
         for line in file.readlines():
             table.append(line.strip())
             data.append([-1]*len(table[-1]))
             output.append(list(" "*len(table[-1])))
             if 'S' in line:
-                start = (rows, line.find('S'))
-            rows += 1
-        steps = 1
-        row, col = start
+                row, col = len(table), line.find('S')
+        rows = len(table)
+        cols = len(table[0])
         data[row][col] = 0
-        direction, row, col = initial(table, row, col)
+        for direction in range(4):
+            if 0 <= row+DR[direction] < rows and 0<= col+DC[direction] < cols \
+                    and table[row+DR[direction]][col+DC[direction]] in DIRECTIONS[direction]:
+                row += DR[direction]
+                col += DC[direction]
+                break
+        steps = 1
         while table[row][col] != 'S':
             data[row][col] = steps
-            direction, row, col = follow(table, row, col, direction)
-            steps += 1
+            direction = DIRECTIONS[direction].find(table[row][col])
             if direction<0:
                 break
+            row += DR[direction]
+            col += DC[direction]
+            steps += 1
+            
         circle = 0
         cols = len(table[0])
         for row in range(rows):
@@ -80,7 +68,7 @@ def main():
             for search_col in range(cols):
                 col = search_col
                 if data[row][col] >= 0:
-                    output[row][search_col] = '+'
+                    output[row][search_col] = "+" # table[row][search_col]
                     continue
                 while col<cols and data[row][col]<0:
                     col += 1
@@ -116,8 +104,8 @@ def main():
                 if dcount == 1:
                     output[row][search_col] = 'X'
 
-        # for line in output:
-        #     print(''.join(line))
+        #for line in output:
+        #    print(''.join(line))
         print(f"number of cells inside the loop : {count}")
 
 if __name__ == "__main__":
