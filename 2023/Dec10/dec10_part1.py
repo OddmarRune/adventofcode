e6cbcd30d67a1e022d8bd27f612e567c87b9c0d6
Oddmar Eiksund <oddmar.eiksund@gmail.com>
2023-12-11 10:27:27 +0100

    Documentation and visual improvements

--- 2023/Dec10/dec10_part1.py
+++ 2023/Dec10/dec10_part1.py
@@ -4,9 +4,10 @@ DIRECTIONS = ["|7F ", "L- F", "J -7", " JL|"]
 DR = [-1, 0, 0, 1]
 DC = [ 0,-1, 1, 0]
 
-def main():
+def main(filename):
     """Solution to part 1"""
-    with open('input.txt', encoding="UTF-8") as file:
+    with open(filename, encoding="UTF-8") as file:
+        # Read the pipe chart and find the starting position
         table = []
         for line in file.readlines():
             table.append(line.strip())
@@ -14,6 +15,8 @@ def main():
                 row, col = len(table), line.find('S')
         rows = len(table)
         cols = len(table[0])
+
+        # Find initial direction and move
         for direction in range(4):
             if 0 <= row+DR[direction] < rows and 0 <= col+DC[direction] < cols \
                     and table[row+DR[direction]][col+DC[direction]] in DIRECTIONS[direction]:
@@ -21,6 +24,8 @@ def main():
                 col += DC[direction]
                 break
         steps = 1
+
+        # Follow the loop and count the steps
         while table[row][col] != 'S':
             direction = DIRECTIONS[direction].find(table[row][col])
             if direction<0:
@@ -28,7 +33,7 @@ def main():
             row += DR[direction]
             col += DC[direction]
             steps += 1
-        print(f"steps to the middle of the loop : {int(steps/2)}") # 7173
+        print(f"steps to the middle of the loop : {int(steps/2)}")
 
 if __name__ == "__main__":
-    main()
+    main("input.txt")
--- 2023/Dec10/dec10_part2.py
+++ 2023/Dec10/dec10_part2.py
@@ -1,23 +1,30 @@
 """Advent of Code, 2023, Day 10"""
 
-DIRECTIONS = ["|7FX", "L-XF", "JX-7", "XJL|"]
+DIRECTIONS = ["|7F ", "L- F", "J -7", " JL|"]
 DR = [-1, 0, 0, 1]
 DC = [ 0,-1, 1, 0]
+PIPES = {'|':'║', 'F':'╔', '-':'═', '7':'╗', 'J':'╝', 'L':'╚' }
 
-def main():
+UNDETERMINED = 0
+CLOCKWISE = -1
+COUNTERCLOCKWISE = 1
+
+def main(filename):
     """Solution to part 2"""
-    with open('input.txt', encoding="UTF-8") as file:
+    with open(filename, encoding="UTF-8") as file:
+        # Read the pipe chart and find the starting position
         table = []
-        data = []
-        output = []
         for line in file.readlines():
             table.append(line.strip())
-            data.append([-1]*len(table[-1]))
-            output.append(list(" "*len(table[-1])))
             if 'S' in line:
                 row, col = len(table), line.find('S')
         rows = len(table)
         cols = len(table[0])
+
+        data = [[-1] * cols for _ in range(rows)]
+        output = [[' '] * cols for _ in range(rows)]
+
+        # Find initial direction and move
         data[row][col] = 0
         for direction in range(4):
             if 0 <= row+DR[direction] < rows and 0<= col+DC[direction] < cols \
@@ -26,6 +33,8 @@ def main():
                 col += DC[direction]
                 break
         steps = 1
+
+        # Follow the loop and store step-counter in pipe
         while table[row][col] != 'S':
             data[row][col] = steps
             direction = DIRECTIONS[direction].find(table[row][col])
@@ -35,42 +44,45 @@ def main():
             col += DC[direction]
             steps += 1
 
-        circle = 0
+        # Determine the loop direction
+        loop_direction = UNDETERMINED
         cols = len(table[0])
         for row in range(rows):
             if data[row][0] >=0:
                 continue
             for col in range(cols):
                 if data[row][col] < 0:
-                    continue           
+                    continue 
                 if 0 < row < rows-1:
                     if data[row-1][col] == data[row][col]+1 \
                         or data[row+1][col] == data[row][col]-1:
-                        circle = -1
+                        loop_direction = CLOCKWISE
                     else:
-                        circle = 1
+                        loop_direction = COUNTERCLOCKWISE
                 elif row == 0 and col < len(data[row])-1:
                     if data[row][col+1] == data[row][col]+1 \
                         or data[row+1][col] == data[row][col]-1:
-                        circle = -1
+                        loop_direction = CLOCKWISE
                     else:
-                        circle = 1
+                        loop_direction = COUNTERCLOCKWISE
                 elif row == rows-1 and col < cols-1:
                     if data[row-1][col] == data[row][col]+1 \
                         or data[row][col+1] == data[row][col]-1:
-                        circle = -1
+                        loop_direction = CLOCKWISE
                     else:
-                        circle = 1
-                if circle != 0:
+                        loop_direction = COUNTERCLOCKWISE
+                if loop_direction != UNDETERMINED:
                     break
-            if circle != 0:
+            if loop_direction != UNDETERMINED:
                 break
+        
+        # Find all enclosed cells
         count = 0
         for row in range(rows):
             for search_col in range(cols):
                 col = search_col
                 if data[row][col] >= 0:
-                    output[row][search_col] = "+" # table[row][search_col]
+                    output[row][search_col] = PIPES[table[row][search_col]]
                     continue
                 while col<cols and data[row][col]<0:
                     col += 1
@@ -79,36 +91,36 @@ def main():
                     if 0 < row < rows-1:
                         if data[row-1][col] == data[row][col]+1 \
                             or data[row+1][col] == data[row][col]-1:
-                            if circle == 1:
+                            if loop_direction == COUNTERCLOCKWISE:
                                 dcount = 1
                         else:
-                            if circle == -1:
+                            if loop_direction == CLOCKWISE:
                                 dcount = 1
                     elif row == 0 and col < cols-1:
                         if data[row][col+1] == data[row][col]+1 \
                             or data[row+1][col] == data[row][col]-1:
-                            if circle == 1:
+                            if loop_direction == COUNTERCLOCKWISE:
                                 dcount = 1
                         else:
-                            if circle == -1:
+                            if loop_direction == CLOCKWISE:
                                 dcount = 1
                     elif row == rows-1 and col < cols-1:
                         if data[row-1][col] == data[row][col]+1 \
                             or data[row][col+1] == data[row][col]-1:
-                            if circle == 1:
+                            if loop_direction == COUNTERCLOCKWISE:
                                 dcount = 1
                         else:
-                            if circle == -1:
+                            if loop_direction == CLOCKWISE:
                                 dcount = 1
                 else:
                     dcount = 0
                 count += dcount
                 if dcount == 1:
-                    output[row][search_col] = 'X'
+                    output[row][search_col] = 'I'
 
-        #for line in output:
-        #    print(''.join(line))
+        for line in output:
+            print(''.join(line))
         print(f"number of cells inside the loop : {count}")
 
 if __name__ == "__main__":
-    main()
+    main("input.txt")
